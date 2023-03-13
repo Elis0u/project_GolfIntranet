01fd57c85c1cd6d54f06669dcd4e9088dfc52080
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-13 15:29:16 +0100

    CREATE store manage user

---
+++ client/src/store/slices/user.js
@@ -0,0 +1,22 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        infos: null,
+        isLogged: false,
+    },
+    reducers: {
+        signIn(state, action) {
+            state.infos = action.payload;
+            state.isLogged = true;
+        },
+        signOut(state, action){
+            state.infos = null;
+            state.isLogged = false;
+        }
+    }
+});
+
+export const {signIn, signOut} = userSlice.actions;
+export default userSlice.reducer;
