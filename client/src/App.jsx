23c67f7e2036b039e78508dbceb09ef029feb266
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-13 15:26:18 +0100

    INIT react

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Router from "./Router/Index";
+
+import Header from "./Components/Header/Index";
+
+function App() {
+	return (  
+		<>
+			<Header />
+			<Router />
+		</>
+	);
+}
+
+export default App;
