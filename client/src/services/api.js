f414c6ad8c9f03003db925acdae07e52055e9cb3
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-13 15:29:55 +0100

    CREATE script get data via axios

---
+++ client/src/services/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+async function getUserAuth(url, TOKEN){
+    try{
+        return await axios.get(url, {headers: {"x-access-token" : TOKEN}});
+    } catch(error) {
+        return error
+    }
+}
+
+async function getDatas(url){
+    try{
+        return await axios.get(url);
+    } catch(error) {
+        throw new Error(error)
+    }
+}
+
+async function signup (datas) {
+    try {
+        return await axios.post("/user/signup", datas);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+async function signin (datas) {
+    try {
+        return await axios.post("/user/signin", datas);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export {getUserAuth, getDatas, signup, signin};
