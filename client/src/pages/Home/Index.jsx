0c5a1a3f10fbaa883706c87921440fb6aad0dd8a
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-14 22:54:21 +0100

    UPDATE home page

--- client/src/pages/Home/Index.jsx
+++ client/src/pages/Home/Index.jsx
@@ -1,10 +1,34 @@
+import { AiOutlinePushpin } from "react-icons/ai";
+import style from "./home.module.css";
 
 function Home() {
 
     return (
-		<main>		
+		<main className={style.wrapper}>		
 			
+      <section className={style.ctnWeather}>
+        <h2>Météo</h2>
+      </section>
 
+      <section className={style.ctnPin}>
+        <h2>< AiOutlinePushpin /> Prochain entraînement</h2>
+      </section>
+
+      <section className={style.ctnNews}>
+        <h2>Dernieres actus</h2>
+      </section>
+
+      <section className={style.ctnEvent}>
+        <h2>Prochains évènements</h2>
+      </section>
+
+      <section className={style.ctnChat}>
+        <h2>CHAT</h2>
+      </section>
+
+      <section className={style.ctnPoll}>
+        <h2>Sondages</h2>
+      </section>
 	
 		</main>
     )
