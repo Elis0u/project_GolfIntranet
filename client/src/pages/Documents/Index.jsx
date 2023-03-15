14a0d0d0cd0cb97ab050fb9b60e446f007e44433
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-15 16:37:11 +0100

    ADD pagination system and style

--- client/src/pages/Documents/Index.jsx
+++ client/src/pages/Documents/Index.jsx
@@ -1,9 +1,13 @@
 import {useEffect, useState} from 'react';
 import style from "./document.module.css";
 import { getDatas } from '../../services/api.js';
+import ReactPaginate from 'react-paginate';
 
 function Documents() {
+  
   const [documents, setDocuments] = useState(null);
+  const [pageNumber, setPageNumber] = useState(0);
+  const documentsPerPage = 4;
 
   useEffect(() => {
   
@@ -21,6 +25,13 @@ function Documents() {
       fetchData();
   }, []);
 
+  const pagesVisited = pageNumber * documentsPerPage;
+  const pageCount = documents ? Math.ceil(documents.length / documentsPerPage) : 0;
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   console.log( "All doc -> " , documents);
 
   function getCategory(category) {
@@ -36,24 +47,35 @@ function Documents() {
 
     return (
 		<main className={style.container}>		
-			
+		
             <h2>Documents</h2>
 
             {
                 documents ?
-                    documents.map(d => {
+                  documents.slice(pagesVisited, pagesVisited + documentsPerPage).map(d => {
                         return (
                             <article key={d.id}>
                                 <h3>{d.title}</h3>
                                 <p>{d.content.slice(0, 200)} ...</p>
-                                <p>Ecrit par {d.firstName} {d.lastName} le <time datetime={d.createdAt}>{new Date(d.createdAt).toLocaleDateString()} à {new Date(d.createdAt).toLocaleTimeString()}</time>.</p>
-                                <p className={getCategory(d.label)}>{d.label}</p>
+                                <p className={style.publish}>Ecrit par {d.firstName} {d.lastName} le <time dateTime={d.createdAt}>{new Date(d.createdAt).toLocaleDateString()} à {new Date(d.createdAt).toLocaleTimeString()}</time>.</p>
+                                <p className={`${style.category} ${getCategory(d.label)}`}>{d.label}</p>
                             </article>
                         )
                     })
                     :
                     null
             }
+                  <ReactPaginate
+                    previousLabel={"<<"}
+                    nextLabel={">>"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={style.pagination}
+                    previousLinkClassName={style.previousPage}
+                    nextLinkClassName={style.nextPage}
+                    disabledClassName={style.paginationDisabled}
+                    activeClassName={style.paginationActive}
+                  />
 	
 		</main>
     )
