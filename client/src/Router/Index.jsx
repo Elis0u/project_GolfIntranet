9d091434cbe308502b8c90cc2dcf0105d819ce2f
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-16 17:45:54 +0100

    ADD team route

--- client/src/Router/Index.jsx
+++ client/src/Router/Index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Index";
 import Documents from "../pages/Documents/Index";
+import Team from "../pages/Team/Index";
 
 
 function Router() {
@@ -8,6 +9,7 @@ function Router() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/documents" element={<Documents />} />
+            <Route path="/team" element={<Team />} />
             {/* 
             <Route path="/tea/:id" element={<SingleTea />} />
             <Route path="/about" element={<About />} />
