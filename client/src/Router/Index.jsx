f559fbbf1219c897cfd6f98b2b2bae76546597d4
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-26 22:08:59 +0200

    ADD HOC auth

--- client/src/Router/Index.jsx
+++ client/src/Router/Index.jsx
@@ -3,15 +3,17 @@ import Home from "../pages/Home/Index";
 import Documents from "../pages/Documents/Index";
 import Team from "../pages/Team/Index";
 import Calendar from "../pages/Events/Index";
-
+import Entry from "../pages/User/Index";
+import HOCAuth from "../helpers/HOC/Auth";
 
 function Router() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/documents" element={<Documents />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/event" element={<Calendar />} />
+            <Route path="/" element={<HOCAuth child={Home} auth />} />
+            <Route path="/documents" element={<HOCAuth child={Documents} auth />} />
+            <Route path="/team" element={<HOCAuth child={Team} auth />} />
+            <Route path="/event" element={<HOCAuth child={Calendar} auth />} />
+            <Route path="/entry" element={<Entry />} />
             {/* 
             <Route path="/tea/:id" element={<SingleTea />} />
             <Route path="/about" element={<About />} />
