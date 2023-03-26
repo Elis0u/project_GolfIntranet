2c9dc3b287035714c81b971d262abc27d289d0f8
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-26 22:09:48 +0200

    FIX sign out check token

--- client/src/helpers/HOC/Auth.jsx
+++ client/src/helpers/HOC/Auth.jsx
@@ -1,32 +1,37 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getUserAuth } from '../../services/api.js';
+import { useDispatch } from "react-redux";
+import { signOut } from "../../store/slices/user";
+import { getUserAuth } from "../../services/api.js";
 
-function HOCAuth({child, auth}) {
-    const navigate = useNavigate();
-	const [isAuthorized, setIsAuthorized] = useState(false);
+function HOCAuth({ child, auth }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
-    const Child = child;
+  const Child = child;
 
-    useEffect(() => {
-        async function checkAuth(){
-			if(auth){
-				const TOKEN = localStorage.getItem("auth");
-				let res = null;
-				if(TOKEN){
-					res = await getUserAuth("/user/checkToken", TOKEN);
-					console.log(res)
-					if(res.status === 200) setIsAuthorized(true);
-				}
-				if(res.code || !TOKEN) navigate("/");
-			}
-            
+  useEffect(() => {
+    async function checkAuth() {
+      if (auth) {
+        const TOKEN = localStorage.getItem("auth");
+        let res = null;
+        if (TOKEN) {
+          res = await getUserAuth("/user/checkToken", TOKEN);
+          console.log(res);
+          if (res.status === 200) setIsAuthorized(true);
         }
-        checkAuth();
-    });
-	
-	if(isAuthorized) return <Child />
-	
+        if (res.code || !TOKEN) {
+          dispatch(signOut());
+          navigate("/entry");
+        }
+      }
+    }
+    checkAuth();
+  });
+
+  if (isAuthorized) return <Child />;
+  return null;
 }
 
 export default HOCAuth;
