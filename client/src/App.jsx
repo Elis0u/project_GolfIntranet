12ba93ed6ddcec94bd2060357fee1fc7afb9d7c5
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-26 22:07:00 +0200

    ADD access header and function checkToken

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Router from "./Router/Index";
 
 import Header from "./Components/Header/Index";
+import { signIn } from "./store/slices/user";
+import { getUserAuth } from "./services/api.js";
 
 function App() {
-	return (  
-		<>
-			<Header />
-			<Router />
-		</>
-	);
+  const isLogged = useSelector((state) => state.user.isLogged);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function checkAuth() {
+      const TOKEN = localStorage.getItem("auth");
+      if (TOKEN) {
+        const res = await getUserAuth("/user/checkToken", TOKEN);
+        if (res.status === 200) {
+          dispatch(signIn(res.data.email));
+        }
+      }
+    }
+    checkAuth();
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLogged ? <Header /> : ""}
+      <Router />
+    </>
+  );
 }
 
 export default App;
