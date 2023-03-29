c3bf130f8e631087c1794a53f262d8d6c6ed2bdb
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-29 18:02:39 +0200

    ADD new input for register, new function focus and blur

--- client/src/pages/User/signForm.jsx
+++ client/src/pages/User/signForm.jsx
@@ -19,12 +19,17 @@ function SignForm() {
         password: "",
         firstName: "",
         lastName: "",
+        birthDate: "",
+        phone: "",
+        handicap: "",
+        termsAccepted: false,
     });
-    const { email, password, firstName, lastName } = inputs;
+    const { email, password, firstName, lastName, birthDate, phone, handicap, termsAccepted } = inputs;
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setInputs({ ...inputs, [name]: value });
+        const newValue = e.target.type === 'checkbox' ? e.target.checked : value;
+        setInputs({ ...inputs, [name]: newValue });
     };
 
     const handleSubmit = (e) => {
@@ -38,7 +43,7 @@ function SignForm() {
 
     const handleSignIn = async () => {
         try {
-            const res = await signin(inputs); // envoyer des inputs "sains/nettoyés"
+            const res = await signin(inputs);
             localStorage.setItem("auth", res.data.result.TOKEN);
             localStorage.setItem("user", JSON.stringify({
                 email: res.data.result.email,
@@ -63,8 +68,9 @@ function SignForm() {
         try {
             const res = await signup(inputs);
             if (res.status === 201) {
-                setInputs({ email: "", password: "", firstName: "", lastName: "" });
+                setInputs({ email: "", password: "", firstName: "", lastName: "", birthDate: "", phone: "", handicap: "", termsAccepted: false });
                 navigate("/entry", { state: { type: "se connecter" } });
+                setMsg("Votre compte à bien été crée, la validation peut prendre jusqu'à 24h");
             }
         } catch (err) {
             console.error(err);
@@ -84,10 +90,21 @@ function SignForm() {
         }
     };
 
+    const setDefaultDate = (e) => {
+        if (e.target.value === "") {
+            e.target.value = "2010-01-01";
+        }
+    };
+
     return (
         <>
-            <form className={style.signForm} onSubmit={handleSubmit}>
+            {type === "se connecter" ? (
+                <h2>Connexion</h2>
+            ) : (
+                <h2>Inscription</h2>
+            )}
 
+            <form className={style.signForm} onSubmit={handleSubmit}>
                 <div className={style.inputGroup}>
                     <label htmlFor="email" className={style.signForm_label}>
                         Email
@@ -153,6 +170,75 @@ function SignForm() {
                                 required
                             />
                         </div>
+
+                        <div className={style.inputGroup}>
+                            <label htmlFor="birthDate" >Date d'anniversaire</label>
+                            <input
+                                type="date"
+                                name="birthDate"
+                                id="birthDate"
+                                value={birthDate}
+                                className={style.signForm_input}
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                                onChange={handleInputChange}
+                                onClick={setDefaultDate}
+                                required
+                            />
+                        </div>
+
+                        <div className={style.inputGroup}>
+                            <label htmlFor="phone">Numéro de téléphone</label>
+                            <input
+                                type="tel"
+                                name="phone"
+                                id="phone"
+                                value={phone}
+                                className={style.signForm_input}
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                                onChange={handleInputChange}
+                                pattern="^(?:0\d\s?){1}\d{2}(?:\s?\d{2}){3}$"
+                                placeholder="06 00 00 00 00"
+                                required
+                            />
+                        </div>
+
+                        <div className={style.inputGroup}>
+                            <label htmlFor="handicap" className={style.signForm_label}>Handicap</label>
+                            <input
+                                type="text"
+                                name="handicap"
+                                id="handicap"
+                                value={handicap}
+                                className={style.signForm_input}
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+
+                        <div className={`${style.inputGroup} ${style.termsAccepted}`}>
+                            <input
+                                type="checkbox"
+                                name="termsAccepted"
+                                id="termsAccepted"
+                                checked={termsAccepted}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <label htmlFor="termsAccepted" >
+                                J'accepte les{" "}
+                                <a href="/politique-confidentialite" target="_blank" rel="noopener noreferrer">
+                                    politiques de confidentialité
+                                </a>{" "}
+                                et les{" "}
+                                <a href="/mentions-legales" target="_blank" rel="noopener noreferrer">
+                                    mentions légales
+                                </a>
+                            </label>
+                        </div>
                     </>
                 )}
 
@@ -161,7 +247,7 @@ function SignForm() {
 
             {msg && <p>{msg}</p>}
 
-            {type === "se connecter" && (
+            {type === "se connecter" ? (
                 <p>
                     Vous n'avez pas de compte ? {" "}
                     <Link
@@ -169,6 +255,17 @@ function SignForm() {
                         state={{ type: "s'enregistrer" }}
                     >
                         Inscrivez-vous
+                    </Link>
+                </p>
+            )
+            : (
+                <p>
+                    Déjà un compte ? {" "}
+                    <Link
+                        to={"/entry"}
+                        state={{ type: "se connecter" }}
+                    >
+                        Connectez-vous
                     </Link>
                 </p>
             )}
