69eae373c6383438e6ec17d6bcd682a268704917
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-03-31 23:34:09 +0200

    FIX all query - remove isFinished

--- server/src/controllers/events.js
+++ server/src/controllers/events.js
@@ -4,7 +4,7 @@ import Query from "../model/query.js";
 // Create
 export const add_event = async (req,res) => {
     try {
-        const query = "INSERT INTO event (title, location, startEvent, endEvent, createdAt, isFinished, category_id, user_id) VALUES (?, ?, ?, ?, NOW(), 0, ?, ?)";
+        const query = "INSERT INTO event (title, location, startEvent, endEvent, createdAt, category_id, user_id) VALUES (?, ?, ?, ?, NOW(), ?, ?)";
         const [result] = await Query.write(query, req.body);
 
         if(result.affectedRows){
@@ -20,7 +20,7 @@ export const add_event = async (req,res) => {
 // Read
 export const all = async (req, res) => {
     try {
-        const query = "SELECT event.id, title, location, startEvent, endEvent, createdAt, isFinished, category_id, user_id FROM event";
+        const query = "SELECT event.id, title, location, startEvent, endEvent, createdAt, category_id, user_id FROM event";
         const [events] = await Query.find(query);
         if(events.length){
             const msg = "Recovery of all events";
@@ -36,7 +36,7 @@ export const all = async (req, res) => {
 
 export const one = async (req, res) => {
     try {
-        const queryEvent = "SELECT event.id, title, location, startEvent, endEvent, createdAt, isFinished, category_id FROM event WHERE id = ?";
+        const queryEvent = "SELECT event.id, title, location, startEvent, endEvent, createdAt, category_id FROM event WHERE id = ?";
         const event = await Query.findByValue(queryEvent, req.params.val);
 
         if(!event.length){
@@ -54,7 +54,7 @@ export const one = async (req, res) => {
 // Update
 export const update = async (req,res) => {
     try {
-        const query = "UPDATE event SET title = ?, location = ?, startEvent = ?, endEvent = ?, isFinished = 0, category_id = ? WHERE id = ?";
+        const query = "UPDATE event SET title = ?, location = ?, startEvent = ?, endEvent = ?, category_id = ? WHERE id = ?";
         const [result] = await Query.write(query, req.body);
 
         if(result.affectedRows){
