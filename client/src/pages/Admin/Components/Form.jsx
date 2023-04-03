ecddebd14815ea3e6a95b4f388189c70fdb3baf5
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-04 00:28:03 +0200

    ADD new condition (event)

--- client/src/pages/Admin/Components/Form.jsx
+++ client/src/pages/Admin/Components/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DocumentForm from './Form/DocumentForm';
 import CategoriesForm from './Form/CategoriesForm';
+import EventForm from './Form/EventForm';
 
 const DynamicForm = ({ activeNavItem, isEditMode, initialData, onSubmitSuccess }) => {
   const renderForm = () => {
@@ -11,6 +12,8 @@ const DynamicForm = ({ activeNavItem, isEditMode, initialData, onSubmitSuccess }
         return <CategoriesForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess} url="/categoriesdocument" />;
       case 'Catégories Evènements':
         return <CategoriesForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess} url="/categoriesevent" />;
+      case 'Evènements':
+        return <EventForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess}/>;
       default:
         return <p>Veuillez sélectionner une catégorie valide.</p>;
     }
