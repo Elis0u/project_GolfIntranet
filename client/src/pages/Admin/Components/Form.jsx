e2685d11de9d8aeec04c15115cc5b5c4cd4faea3
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-03 17:21:54 +0200

    ADD new case on switch for categories

--- client/src/pages/Admin/Components/Form.jsx
+++ client/src/pages/Admin/Components/Form.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import DocumentForm from './Form/DocumentForm';
+import CategoriesForm from './Form/CategoriesForm';
 
 const DynamicForm = ({ activeNavItem, isEditMode, initialData, onSubmitSuccess }) => {
   const renderForm = () => {
     switch (activeNavItem.name) {
       case 'Documents':
         return <DocumentForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess} />;
-        break;
-      case 'Evènements':
-        console.log("Formulaire event")
-        break;
+      case 'Catégories Documents':
+        return <CategoriesForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess} url="/categoriesdocument" />;
+      case 'Catégories Evènements':
+        return <CategoriesForm isEditMode={isEditMode} initialData={initialData} onSubmitSuccess={onSubmitSuccess} url="/categoriesevent" />;
       default:
         return <p>Veuillez sélectionner une catégorie valide.</p>;
     }
