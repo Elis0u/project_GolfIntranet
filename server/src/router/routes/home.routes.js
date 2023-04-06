2dd43b9479f114d7c0099e66989804d1327ed620
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-06 23:52:01 +0200

    CREATE new home route

--- server/src/router/routes/home.routes.js
+++ server/src/router/routes/home.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import { lastActivities } from '../../controllers/home.js'
+import { lastActivities, nextEvent, fourEvents } from '../../controllers/home.js'
 
 const router = Router();
 
 router.get("/lastActivities", lastActivities);
+router.get("/nextEvent", nextEvent);
+router.get("/fourEvents", fourEvents);
 
 export default router;
