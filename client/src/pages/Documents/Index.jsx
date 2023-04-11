670236179ffcc1208cc838e84b5a43648812e062
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-11 11:49:43 +0200

    ADD Modal to show detail document

--- client/src/pages/Documents/Index.jsx
+++ client/src/pages/Documents/Index.jsx
@@ -1,28 +1,33 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import style from "./document.module.css";
 import { getDatas } from '../../services/api.js';
 import ReactPaginate from 'react-paginate';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 function Documents() {
-  
+
   const [documents, setDocuments] = useState(null);
   const [pageNumber, setPageNumber] = useState(0);
   const documentsPerPage = 4;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedDocument, setSelectedDocument] = useState(null);
 
   useEffect(() => {
-  
-      async function fetchData(){
-          try{
-              const documents = await getDatas("/documents");
-  
-              setDocuments(documents.data.result);
 
-          } catch(err){
-              throw new Error(err);
-          }
-          
+    async function fetchData() {
+      try {
+        const documents = await getDatas("/documents");
+
+        setDocuments(documents.data.result);
+
+      } catch (err) {
+        throw new Error(err);
       }
-      fetchData();
+
+    }
+    fetchData();
   }, []);
 
   const pagesVisited = pageNumber * documentsPerPage;
@@ -32,7 +37,7 @@ function Documents() {
     setPageNumber(selected);
   };
 
-  console.log( "All doc -> " , documents);
+  console.log("All doc -> ", documents);
 
   function getCategory(category) {
     switch (category) {
@@ -45,40 +50,76 @@ function Documents() {
     }
   }
 
-    return (
-		<main className={style.container}>		
-		
-            <h2>Documents</h2>
+  const openModal = (document) => {
+    setSelectedDocument(document);
+    setModalIsOpen(true);
+  };
 
-            {
-                documents ?
-                  documents.slice(pagesVisited, pagesVisited + documentsPerPage).map(d => {
-                        return (
-                            <article key={d.id}>
-                                <h3>{d.title}</h3>
-                                <p>{d.content.slice(0, 200)} ...</p>
-                                <p className={style.publish}>Ecrit par {d.firstName} {d.lastName} le <time dateTime={d.createdAt}>{new Date(d.createdAt).toLocaleDateString()} à {new Date(d.createdAt).toLocaleTimeString()}</time>.</p>
-                                <p className={`${style.category} ${getCategory(d.label)}`}>{d.label}</p>
-                            </article>
-                        )
-                    })
-                    :
-                    null
-            }
-                  <ReactPaginate
-                    previousLabel={"<<"}
-                    nextLabel={">>"}
-                    pageCount={pageCount}
-                    onPageChange={changePage}
-                    containerClassName={style.pagination}
-                    previousLinkClassName={style.previousPage}
-                    nextLinkClassName={style.nextPage}
-                    disabledClassName={style.paginationDisabled}
-                    activeClassName={style.paginationActive}
-                  />
-	
-		</main>
-    )
+  const closeModal = () => {
+    setSelectedDocument(null);
+    setModalIsOpen(false);
+  };
+
+  return (
+    <main className={style.container}>
+
+      <h2>Documents</h2>
+
+      {
+        documents ?
+          documents.slice(pagesVisited, pagesVisited + documentsPerPage).map(d => {
+            return (
+              <article key={d.id} onClick={() => openModal(d)}>
+                <h3>{d.title}</h3>
+                <p>{d.content.slice(0, 200)} ...</p>
+                <p className={style.publish}>Ecrit par {d.firstName} {d.lastName} le <time dateTime={d.createdAt}>{new Date(d.createdAt).toLocaleDateString()} à {new Date(d.createdAt).toLocaleTimeString()}</time>.</p>
+                <p className={`${style.category} ${getCategory(d.label)}`}>{d.label}</p>
+              </article>
+            )
+          })
+          :
+          null
+      }
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Document Details"
+        className={style.modalContent}
+        overlayClassName={style.modalOverlay}
+      >
+        {selectedDocument && (
+          <div>
+            <h3>{selectedDocument.title}</h3>
+            <p>{selectedDocument.content}</p>
+            <p className={style.publish}>
+              Ecrit par {selectedDocument.firstName} {selectedDocument.lastName} le{" "}
+              <time dateTime={selectedDocument.createdAt}>
+                {new Date(selectedDocument.createdAt).toLocaleDateString()} à{" "}
+                {new Date(selectedDocument.createdAt).toLocaleTimeString()}
+              </time>
+              .
+            </p>
+            <p className={`${style.category} ${getCategory(selectedDocument.label)}`}>
+              {selectedDocument.label}
+            </p>
+          </div>
+        )}
+        <button onClick={closeModal}>Fermer</button>
+      </Modal>
+      <ReactPaginate
+        previousLabel={"<<"}
+        nextLabel={">>"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={style.pagination}
+        previousLinkClassName={style.previousPage}
+        nextLinkClassName={style.nextPage}
+        disabledClassName={style.paginationDisabled}
+        activeClassName={style.paginationActive}
+      />
+
+    </main>
+  )
 }
 
 export default Documents;
