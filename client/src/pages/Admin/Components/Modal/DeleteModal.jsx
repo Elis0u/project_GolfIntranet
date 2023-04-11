a2055884594048a370174e5c267e327c988f3604
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-11 21:53:16 +0200

    CREATE modal component DELETE

---
+++ client/src/pages/Admin/Components/Modal/DeleteModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Modal from 'react-modal';
+import style from '../../admin.module.css';
+
+function DeleteModal({ modalIsOpen, closeModal, handleDelete }) {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Suppression de l'élément"
+      className={style.modalContentDelete}
+      overlayClassName={style.modalOverlay}
+      role="dialog"
+      aria-modal="true"
+    >
+      <h2>Confirmation de la suppression</h2>
+      <p>Êtes-vous sûr de vouloir supprimer cet élément ? Cette action est irréversible.</p>
+      <div className={style.modalActions}>
+        <button onClick={closeModal}>Annuler</button>
+        <button onClick={handleDelete} className={style.deleteButton}>
+          Supprimer
+        </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default DeleteModal;
