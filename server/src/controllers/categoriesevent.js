0126608b576d9fb4bdade4d1b3d1cdbccc9b8026
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-12 16:17:45 +0200

    UPDATE : add status code

--- server/src/controllers/categoriesevent.js
+++ server/src/controllers/categoriesevent.js
@@ -2,18 +2,21 @@ import { success } from "../helpers/index.js";
 import Query from "../model/query.js";
 
 // Create
-export const add = async (req,res) => {
+export const add = async (req, res) => {
     try {
         const query = "INSERT INTO categoryevent (label) VALUES (?)";
         const [result] = await Query.write(query, req.body);
-        
-        if(result.affectedRows){
+
+        if (result.affectedRows) {
             const msg = "Event category added";
-            res.json(success(msg, result));
-        } else throw Error("Event category couldn't be added, probably syntax error in object");
-        
+            res.status(201).json(success(msg, result));
+        } else {
+            const msg = "Event category couldn't be added, probably syntax error in object";
+            res.status(400).json({ error: msg });
+        }
+
     } catch (err) {
-        throw Error(err);
+        res.status(500).json({ err: 'An error occurred while processing your request.' });
     }
 }
 
@@ -23,7 +26,7 @@ export const all = async (req, res) => {
         const query = "SELECT categoryevent.id, label FROM categoryevent";
         const [categories] = await Query.find(query);
 
-        if(categories.length){
+        if (categories.length) {
             const msg = "Recovery of all event categories";
             res.status(200).json(success(msg, categories));
         } else {
@@ -31,7 +34,7 @@ export const all = async (req, res) => {
             res.status(200).json(success(msg));
         }
     } catch (err) {
-        throw Error(err);
+        res.status(500).json({ err: 'An error occurred while processing your request.' });
     }
 }
 
@@ -39,8 +42,8 @@ export const one = async (req, res) => {
     try {
         const query = "SELECT categoryevent.id, label FROM categoryevent WHERE id = ?";
         const category = await Query.findOne(query, req.params.id);
-        
-        if(!category.length){
+
+        if (!category.length) {
             const msg = "This event category does not exist in database";
             res.status(200).json(success(msg));
         } else {
@@ -48,40 +51,46 @@ export const one = async (req, res) => {
             res.status(200).json(success(msg, category));
         }
     } catch (err) {
-        throw Error(err);
+        res.status(500).json({ err: 'An error occurred while processing your request.' });
     }
 }
 
 // Update
-export const update = async (req,res) => {
+export const update = async (req, res) => {
     try {
         const query = "UPDATE categoryevent SET label = ? WHERE id = ?";
         const [result] = await Query.write(query, req.body);
 
-        if(result.affectedRows){
+        if (result.affectedRows) {
             const msg = "Event category updated";
-            res.json(success(msg));
+            res.status(200).json(success(msg));
 
-        } else throw Error("Event category couldn't be updated, probably syntax error in object");
-        
+        } else {
+            const msg = "Document category couldn't be updated, probably syntax error in object";
+            res.status(400).json({ error: msg });
+        }
+
     } catch (err) {
-        throw Error(err);
+        res.status(500).json({ err: 'An error occurred while processing your request.' });
     }
 }
 
 // Delete
-export const remove = async (req,res) => {
+export const remove = async (req, res) => {
     try {
         const query = "DELETE FROM categoryevent WHERE id = ?";
-        const [ result ] = await Query.remove(query, req.body.id);
-        
-        if(result.affectedRows){
-            const msg = "Event category removed";
-            res.json(success(msg));
+        const [result] = await Query.remove(query, req.body.id);
 
-        } else throw Error("Event category couldn't be removed, probably syntax error in object");
+        if (result.affectedRows) {
+            const msg = "Event category removed";
+            res.status(200).json(success(msg));
+
+        } else {
+            const msg = "Document category couldn't be removed, probably syntax error in object";
+            res.status(400).json({ error: msg });
+        }
 
     } catch (err) {
-        throw Error(err);
+        res.status(500).json({ err: 'An error occurred while processing your request.' });
     }
 }
