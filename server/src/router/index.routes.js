ba65577b273bdb780f85bcf6ab21af086a323ace
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-12 17:35:25 +0200

    DELETE polls route

--- server/src/router/index.routes.js
+++ server/src/router/index.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 
 import documents_routes from "./routes/documents.routes.js";
 import categoriesdocument_routes from "./routes/categoriesdocument.routes.js";
-import polls_routes from "./routes/polls.routes.js";
 import events_routes from "./routes/events.routes.js";
 import categoriesevent_routes from "./routes/categoriesevent.routes.js";
 import pelz_routes from "./routes/pelz.routes.js";
@@ -15,7 +14,7 @@ const router = Router();
 router.use("/documents", documents_routes);
 router.use("/categoriesdocument", categoriesdocument_routes);
 
-router.use("/polls", polls_routes);
+
 
 router.use("/events", events_routes);
 router.use("/categoriesevent", categoriesevent_routes);
