ad9c57b5d62f7cc7b38e35a71c6808f0113e64dc
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-18 14:30:14 +0200

    FIX if user isn't connected, navigate to /entry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import Router from "./Router/Index";
 
 import Header from "./Components/Header/Index";
@@ -9,6 +10,7 @@ import { getUserAuth } from "./services/api.js";
 function App() {
   const isLogged = useSelector((state) => state.user.isLogged);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function checkAuth() {
@@ -18,14 +20,20 @@ function App() {
         const res = await getUserAuth("/user/checkToken", TOKEN);
         if (res.status === 200) {
           dispatch(signIn(res.data.email));
+        } else {
+          navigate('/entry');
         }
+      } else {
+        navigate('/entry');
       }
       if (user) {
         dispatch(signIn(JSON.parse(user)));
+      } else {
+        navigate('/entry');
       }
     }
     checkAuth();
-  }, [dispatch]);
+  }, [dispatch, navigate]);
 
   return (
     <>
