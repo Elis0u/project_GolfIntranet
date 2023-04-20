6a53707df2c2985c2462dde49a89735318d2c1eb
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-20 17:03:29 +0200

    ADD activities section

--- client/src/pages/User/AccountPage.jsx
+++ client/src/pages/User/AccountPage.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import style from "./accountPage.module.css";
 import { AiOutlineEdit } from "react-icons/ai";
+import { getDatas } from '../../services/api.js';
 
 function AccountPage() {
   const user = useSelector((state) => state.user.infos);
+  const [activitiesUser, setActivitiesUser] = useState(null);
+
+  useEffect(() => {
+    if (user.id) {
+      async function fetchData() {
+        try {
+          const activitiesUser = await getDatas(`/user/activities_user?userId=${user.id}`);
+          setActivitiesUser(activitiesUser.data.result);
+        } catch (error) {
+          throw new Error(error);
+        }
+      }
+      fetchData();
+    }
+  }, [user.id]);
+
+  const renderUserActivity = (activity) => {
+    const localDate = new Date(activity.createdAt).toLocaleString();
+    const uniqueKey = `${activity.activity_type}-${activity.id}`;
+  
+    switch (activity.activity_type) {
+      case 'document':
+        return (
+          <li key={uniqueKey} className={style.activityItem}>
+            {localDate} | Vous avez publié le document : {activity.title}
+          </li>
+        );
+      case 'event':
+        return (
+          <li key={uniqueKey} className={style.activityItem}>
+            {localDate} | Vous avez planifié un évènement : {activity.title}
+          </li>
+        );
+      default:
+        return (
+          <li>
+            Aucune activités de votre côté
+          </li>
+        );
+    }
+  };
 
   return (
     <main className={style.ctnDashboard}>
@@ -40,8 +82,11 @@ function AccountPage() {
 
         <section className={style.activityUser}>
           <h3>Vos activités</h3>
+          <ul className={style.activitiesList}>
+            {activitiesUser ? activitiesUser.map((a) => renderUserActivity(a)) : null}
+          </ul>
         </section>
-        
+
       </div>
 
       <section>
