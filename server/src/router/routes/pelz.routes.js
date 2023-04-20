56c25e42b83cb82dea50dfd57dc0708c72bd4816
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-20 19:53:39 +0200

    ADD new route for pelz score

--- server/src/router/routes/pelz.routes.js
+++ server/src/router/routes/pelz.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 
-import { all, one, add_pelz, update, remove } from '../../controllers/pelz.js'
+import { allByUser, add_pelz, update, remove } from '../../controllers/pelz.js'
 
 const router = Router();
 
-router.get("/", all);
-router.get("/:val", one);
+router.get("/allPelzScoreByUser", allByUser);
 
 router.post("/", add_pelz);
 router.put("/", update);
