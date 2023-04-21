4327b34f930bfa26f753ac11929de0c53585b7b2
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-21 20:35:53 +0200

    OPTIMIZATION : remove function

--- client/src/pages/Events/Index.jsx
+++ client/src/pages/Events/Index.jsx
@@ -5,6 +5,8 @@ import { getDatas } from "../../services/api.js";
 import style from "./calendar.module.css";
 import { IoLocationOutline } from "react-icons/io5";
 import { IoGolfOutline } from "react-icons/io5";
+import { formatDateAndTime, isToday } from './utils';
+import  GenerateCalendar  from "./Components/GenerateCalendar.jsx"
 
 function Calendar() {
   const [events, setEvents] = useState([]);
@@ -22,61 +24,6 @@ function Calendar() {
     fetchData();
   }, []);
 
-
-  // Fonction pour afficher les événements sur le calendrier
-  const renderEvents = (date) => {
-    const eventList = events.filter((event) => {
-      const eventStartDate = moment(event.startEvent);
-      const eventEndDate = moment(event.endEvent);
-      return date.isBetween(eventStartDate, eventEndDate, "day", "[]");
-    });
-    return (
-      <ul>
-        {eventList.map((event) => {
-          const eventStartDate = moment(event.startEvent);
-          const eventEndDate = moment(event.endEvent);
-          const isMultiDayEvent = !eventStartDate.isSame(eventEndDate, "day");
-          return (
-            <li key={event.id}>
-              <span>
-                {eventStartDate.isSame(date, "day") ? moment(event.startEvent).format("LT") : ""}
-                {!isMultiDayEvent ? ` - ${moment(event.endEvent).format("LT")}` : ""}
-                {isMultiDayEvent && eventEndDate.isSame(date, "day") ? ` Fini à ${moment(event.endEvent).format("LT")}` : ""}
-              </span>
-              <span>{event.title}</span>
-              <span>{event.description}</span>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  };
-
-  function isToday(date) {
-    return moment().isSame(moment(date), "day");
-  }
-  // Générer les jours du calendrier
-  const generateCalendar = () => {
-    const calendar = [];
-    if (events.length > 0) {
-      const startDate = moment(currentDate)
-        .startOf("month")
-        .startOf("week");
-      const endDate = moment(currentDate).endOf("month").endOf("week");
-      let day = startDate;
-      while (day.isBefore(endDate)) {
-        calendar.push(
-          <div key={day.format("L")} className={style.day} >
-            <span className={`${isToday(day) ? style.today : " "}`}>{day.format("D")}</span>
-            {renderEvents(day)}
-          </div>
-        );
-        day = day.add(1, "day");
-      }
-    }
-    return calendar;
-  };
-
   const handlePrevMonth = () => {
     setCurrentDate(moment(currentDate).subtract(1, "month"));
   };
@@ -84,17 +31,6 @@ function Calendar() {
   const handleNextMonth = () => {
     setCurrentDate(moment(currentDate).add(1, "month"));
   };
-
-  function formatDateAndTime(start, end) {
-    const startDate = new Date(start);
-    const endDate = new Date(end);
-
-    if (startDate.toLocaleDateString() === endDate.toLocaleDateString()) {
-      return `${startDate.toLocaleDateString()} ${startDate.toLocaleTimeString()} - ${endDate.toLocaleTimeString()}`;
-    } else {
-      return `${startDate.toLocaleString()} - ${endDate.toLocaleString()}`;
-    }
-  }
 
   return (
     <>
@@ -115,7 +51,7 @@ function Calendar() {
             <span>Samedi</span>
             <span>Dimanche</span>
           </div>
-          <div className={style.days}>{generateCalendar()}</div>
+          <div className={style.days}>{GenerateCalendar(events, currentDate)}</div>
         </section>
 
         <section className={style.eventMobil}>
