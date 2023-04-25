89ef8802dff9db8451b4fc351c429b0190c0d86c
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-25 21:16:26 +0200

    UPDATE balise html & ADD module style

--- client/src/pages/Others/LegalMentions.jsx
+++ client/src/pages/Others/LegalMentions.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import style from "./othersPage.module.css";
 
 function LegalMentions() {
     return (
-        <main>
-            <div>
-                <h1>Mentions légales</h1>
-                <h2>Éditeur du site</h2>
+        <main className={style.ctnOtherPage}>
+            <section>
+                <h2>Mentions légales</h2>
+                <h3>Éditeur du site</h3>
                 <p>
                     Nom de la société : //<br />
                     Adresse : //<br />
@@ -22,21 +23,21 @@ function LegalMentions() {
                     Email : //
                 </p>
 
-                <h2>Propriété intellectuelle</h2>
+                <h3>Propriété intellectuelle</h3>
                 <p>
                     Tous les contenus présents sur ce site (textes, images, vidéos, etc.) sont soumis au droit d'auteur et sont la propriété exclusive de la société Exemple. Toute reproduction, représentation ou diffusion, totale ou partielle, des éléments présents sur ce site, sans l'accord préalable de la société Exemple, est interdite.
                 </p>
 
-                <h2>Responsabilité</h2>
+                <h3>Responsabilité</h3>
                 <p>
                     La société Exemple ne saurait être tenue pour responsable en cas d'erreurs, d'omissions ou de défauts de mise à jour des informations contenues sur ce site. La société Exemple se réserve le droit de modifier, de corriger ou de supprimer tout ou partie du contenu du site à tout moment et sans préavis.
                 </p>
 
-                <h2>Données personnelles</h2>
+                <h3>Données personnelles</h3>
                 <p>
                     La société Exemple s'engage à protéger les données personnelles de ses utilisateurs conformément à la réglementation en vigueur. Pour en savoir plus sur notre politique de protection des données personnelles, veuillez consulter notre page de politique de confidentialité.
                 </p>
-            </div>
+            </section>
         </main>
 
     );
