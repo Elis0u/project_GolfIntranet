4d826e94fff0ab22bf8c908291b2aa796fea89a4
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-25 20:47:11 +0200

    CREATE legal mention & privacy polity page

---
+++ client/src/pages/Others/LegalMentions.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function LegalMentions() {
+    return (
+        <main>
+            <div>
+                <h1>Mentions légales</h1>
+                <h2>Éditeur du site</h2>
+                <p>
+                    Nom de la société : //<br />
+                    Adresse : //<br />
+                    Téléphone : //<br />
+                    Email : //<br />
+                    SIRET : //
+                </p>
+
+                <h2>Hébergeur du site</h2>
+                <p>
+                    Hébergeur : //<br />
+                    Adresse : //<br />
+                    Téléphone : //<br />
+                    Email : //
+                </p>
+
+                <h2>Propriété intellectuelle</h2>
+                <p>
+                    Tous les contenus présents sur ce site (textes, images, vidéos, etc.) sont soumis au droit d'auteur et sont la propriété exclusive de la société Exemple. Toute reproduction, représentation ou diffusion, totale ou partielle, des éléments présents sur ce site, sans l'accord préalable de la société Exemple, est interdite.
+                </p>
+
+                <h2>Responsabilité</h2>
+                <p>
+                    La société Exemple ne saurait être tenue pour responsable en cas d'erreurs, d'omissions ou de défauts de mise à jour des informations contenues sur ce site. La société Exemple se réserve le droit de modifier, de corriger ou de supprimer tout ou partie du contenu du site à tout moment et sans préavis.
+                </p>
+
+                <h2>Données personnelles</h2>
+                <p>
+                    La société Exemple s'engage à protéger les données personnelles de ses utilisateurs conformément à la réglementation en vigueur. Pour en savoir plus sur notre politique de protection des données personnelles, veuillez consulter notre page de politique de confidentialité.
+                </p>
+            </div>
+        </main>
+
+    );
+}
+
+export default LegalMentions;
