7c347d4d2d1f471cf0b18be9ab30b5bdd07f5561
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-26 11:27:12 +0200

    ADD const api_key

--- server/src/config/const.js
+++ server/src/config/const.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || process.env.LOCAL_PORT ;
 
-export const { HOST, DB_HOST, DB_NAME, DB_USER, DB_PWD } =
+export const { HOST, DB_HOST, DB_NAME, DB_USER, DB_PWD, API_WEATHER_KEY } =
     process.env;
