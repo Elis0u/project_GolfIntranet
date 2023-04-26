ebea19514357d1c6fae62d03dc0d5f5eab322a04
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-26 11:28:25 +0200

    CREATE weather route

--- server/src/router/routes/home.routes.js
+++ server/src/router/routes/home.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import { lastActivities, nextEvent, fourEvents } from '../../controllers/home.js'
+import { lastActivities, nextEvent, fourEvents, weather } from '../../controllers/home.js'
 
 const router = Router();
 
 router.get("/lastActivities", lastActivities);
 router.get("/nextEvent", nextEvent);
 router.get("/fourEvents", fourEvents);
+router.get("/weather", weather);
 
 export default router;
