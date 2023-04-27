c12f00a3c0d5109bedf1ab046217c6e1df4fd253
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-27 21:11:56 +0200

    ADD middleware on update avatar route

--- server/src/router/routes/users.routes.js
+++ server/src/router/routes/users.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import {checkToken, all, activitiesUser, signup, signin, update_isConfirmed, update_isAdmin, update_user, update_avatar, remove_user } from '../../controllers/users.js';
 import { auth } from '../../middlewares/auth.js';
 import upload from '../../middlewares/multerConfig.js';
+import { multerErrorHandler } from '../../middlewares/multerErrorHandler.js';
 
 
 const router = Router();
@@ -18,7 +19,7 @@ router.post("/signin", signin);
 router.put("/update_isConfirmed", update_isConfirmed);
 router.put("/update_isAdmin", update_isAdmin);
 router.put("/update_user", update_user);
-router.put("/update_avatar", auth, upload.single('avatar'), update_avatar);
+router.put("/update_avatar", auth, upload.single('avatar'), multerErrorHandler, update_avatar);
 
 router.delete("/", remove_user);
 
