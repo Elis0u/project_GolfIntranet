f97ca97a77e91745b752264fbe345cb72606722b
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-04-27 18:14:37 +0200

    CREATE update avatar route

--- server/src/router/routes/users.routes.js
+++ server/src/router/routes/users.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
-import {checkToken, all, activitiesUser, signup, signin, update_isConfirmed, update_isAdmin, update_user, remove_user } from '../../controllers/users.js';
+import {checkToken, all, activitiesUser, signup, signin, update_isConfirmed, update_isAdmin, update_user, update_avatar, remove_user } from '../../controllers/users.js';
 import { auth } from '../../middlewares/auth.js';
+import upload from '../../middlewares/multerConfig.js';
 
 
 const router = Router();
@@ -13,9 +14,11 @@ router.get("/activities_user", activitiesUser);
 router.post("/signup", signup);
 router.post("/signin", signin);
 
+
 router.put("/update_isConfirmed", update_isConfirmed);
 router.put("/update_isAdmin", update_isAdmin);
 router.put("/update_user", update_user);
+router.put("/update_avatar", auth, upload.single('avatar'), update_avatar);
 
 router.delete("/", remove_user);
 
